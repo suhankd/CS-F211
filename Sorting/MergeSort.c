449d7208555b9315d00e92c72cdf4de784187db9
Suhan Donthineni <suhanraodonthineni@gmail.com>
2025-01-19 14:32:14 +0530

    Merge Sort

--- Sorting/MergeSort.c
+++ Sorting/MergeSort.c
@@ -13,19 +13,31 @@ int main(int argc, char **argv)
 
     for (int i = 0; i < n; i++)
     {
-        arr[i] = atoi(argv[i]);
-        printf("%d\n", arr[i]);
+        arr[i] = atoi(argv[i + 1]);
     }
+
+    int low = 0;
+    int high = n - 1;
+
+    mergeSort(arr, low, high);
+
+    for (int i = 0; i < n; i++)
+    {
+        printf("%d ", arr[i]);
+    }
+
+    printf("\n");
 
     return 0;
 }
 
-void mergeSort(int arr[], int left, int right)
+void mergeSort(int *arr, int left, int right)
 {
-    if (left > right)
+
+    if (left < right)
     {
 
-        int mid = (left + right) / 2;
+        int mid = left + (right - left) / 2;
 
         mergeSort(arr, left, mid);
         mergeSort(arr, mid + 1, right);
@@ -36,22 +48,61 @@ void mergeSort(int arr[], int left, int right)
 
 void Merge(int *arr, int left, int mid, int right)
 {
-    int k;
 
     int n1 = mid - left + 1;
     int n2 = right - mid;
 
     int leftArr[n1], rightArr[n2];
 
-    for (int i = 0; i < n1; i++){leftArr[i] = arr[left + i];}
-    for (int i = 0; i < n2; i++){rightArr[i] = arr[mid + i + 1];}
+    for (int i = 0; i < n1; i++)
+    {
 
-    int i, j = 0;
-    while(i < n1 && j < n2) {
-
-        if(leftArr[i] <= )
-
+        leftArr[i] = arr[left + i];
     }
 
+    for (int j = 0; j < n2; j++)
+    {
 
+        rightArr[j] = arr[mid + 1 + j];
+    }
+
+    int i = 0;
+    int j = 0;
+
+    int k = left;
+
+    while ((i < n1) && (j < n2))
+    {
+
+        if (leftArr[i] < rightArr[j])
+        {
+
+            arr[k] = leftArr[i];
+            i++;
+        }
+        else
+        {
+
+            arr[k] = rightArr[j];
+            j++;
+        }
+
+        k++;
+    }
+
+    while (i < n1)
+    {
+
+        arr[k] = leftArr[i];
+        i++;
+        k++;
+    }
+
+    while (j < n2)
+    {
+
+        arr[k] = rightArr[j];
+        j++;
+        k++;
+    }
 }
